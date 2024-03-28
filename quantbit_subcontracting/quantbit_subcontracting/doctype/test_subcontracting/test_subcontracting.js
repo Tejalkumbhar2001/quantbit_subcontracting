1d8f68485e0fe297ecf1507c60929fadbda3a837
Tejalkumbhar2001 <tejalkumbhar5258@gmail.com>
2024-03-28 12:24:47 +0530

    xyz

---
+++ quantbit_subcontracting/quantbit_subcontracting/doctype/test_subcontracting/test_subcontracting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, quantbit technologies pvt ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Test Subcontracting', {
+	// refresh: function(frm) {
+
+	// }
+});
